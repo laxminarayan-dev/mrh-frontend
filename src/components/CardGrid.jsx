12a365e05e4408c67ab24a01d4f32171f346291d
Laxmi Narayan <jaiswallucky0112@gmail.com>
2026-02-03 17:31:01 +0530

    Card & Grid Finalize

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,17 +1,25 @@
-import React from "react";
-
 function CardGrid({ children, className = "" }) {
   return (
     <div className="w-full overflow-hidden">
       {/* <div
         className={`mx-auto inline-grid gap-4 px-2 my-8 ${className}`}
         style={{
-          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 200px))",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
           maxWidth: "100%",
         }}
       > */}
       <div
-        className={`mx-auto inline-grid grid-cols-1 min-[410px]:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 my-8 w-[80vw] min-[410px]:w-[100%] px-2 min-[410px]:px-4 ${className}`}>
+        className="
+    grid
+    mt-8
+    px-3
+    gap-4
+    justify-center
+    [grid-template-columns:1fr]
+    min-[440px]:[grid-template-columns:repeat(auto-fit,200px)]
+  "
+      >
+
         {children}
       </div>
     </div>
