b37b69d229ac81be27fb74ad6883b493e604f364
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-02-05 22:54:36 +0530

    image loader bug resolve try 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -6,8 +5,8 @@ import { store } from "./store/store.js";
 
 import Routing from "./Routing.jsx";
 import ScrollToTop from "./components/ScrollToTop.jsx";
-// import "leaflet/dist/leaflet.css";
-// import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
