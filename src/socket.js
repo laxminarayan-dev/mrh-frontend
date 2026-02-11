3bf5051ee22f7162133be74e69ef36c3ebe8e2d5
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-02-11 09:20:36 +0530

    Socket Fix for VPS

--- src/socket.js
+++ src/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:8000";
 
 export const socket = io(SOCKET_URL, {
+    path: "/mrh-backend/socket.io",
     autoConnect: false,
     transports: ["websocket"], // faster & stable
 });
