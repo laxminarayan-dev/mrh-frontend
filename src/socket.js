bbf67b9291c26cbaa3a4f323beac05119cdb5c2e
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-02-11 10:36:55 +0530

    Trial 3

--- src/socket.js
+++ src/socket.js
@@ -4,8 +4,10 @@ const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:8000";
 
 console.log("Connecting to socket at:", SOCKET_URL);
 
-export const socket = io(SOCKET_URL, {
-    path: "/socket.io",
-    autoConnect: false,
-    // transports: ["websocket"], // faster & stable
-});
+export const socket = io(
+    SOCKET_URL,
+    {
+        path: "/mrh-backend/socket.io",
+        autoConnect: false,
+    }
+);
