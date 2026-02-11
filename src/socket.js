b901cb80796b1970a2478ab84567e7f5a55538ac
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-02-11 10:12:39 +0530

    checking

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,8 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:8000";
 
+console.log("Connecting to socket at:", SOCKET_URL);
+
 export const socket = io(SOCKET_URL, {
     path: "/socket.io",
     autoConnect: false,
